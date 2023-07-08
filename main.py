0e5b5fc2face8b389385090fc7894a3f800b51f6
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-07-07 23:58:30 -0700

    can send message as sms doesnt send

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from imessage_reader import fetch_data
 from dotenv import dotenv_values
+import os
 
 config = dotenv_values(".env")
 
 DB_PATH = config["MESSAGEDB"]
+
+RECIPIENT_NUMBER = config["RECIPIENT_NUMBER"] # I suggest adding yours initially to test
+MESSAGE = "'Whats up doc'"
 
 # Create a FetchData instance
 fd = fetch_data.FetchData(DB_PATH)
@@ -12,3 +16,4 @@ fd = fetch_data.FetchData(DB_PATH)
 # This is a list of tuples containing user id, message and service (iMessage or SMS).
 messages = fd.get_messages()
 print(messages[1][1])
+os.system("osascript sendMessage.applescript {} {}".format(RECIPIENT_NUMBER, MESSAGE))
